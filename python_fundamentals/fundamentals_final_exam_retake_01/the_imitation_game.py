4cad0296cefff89a1694ce72ad784a2267e59d0f
Hristo Blagoev <Hristo.H.Blagoev@gmail.com>
2024-03-25 17:34:19 +0000

    Update chars_in_string.py

---
+++ python_fundamentals/fundamentals_final_exam_retake_01/the_imitation_game.py
@@ -0,0 +1,21 @@
+message = input()
+command = input()
+
+while command != "Decode":
+    command = command.split("|")
+    action = command[0]
+
+    if action == "Move":
+        number_of_letters = int(command[1])
+        message = message[number_of_letters:] + message[:number_of_letters]
+    elif action == "Insert":
+        index = int(command[1])
+        value = command[2]
+        message = message[:index] + value + message[index:]
+    elif action == "ChangeAll":
+        substring = command[1]
+        replacement = command[2]
+        message = message.replace(substring, replacement)
+
+    command = input()
+print(f"You have a new text message: {message}")
